b5abf6c81a1ab08eb28715a024e78e463e245574
CarlosTorresZ <jtorres140@outlook.com>
2022-02-05 12:00:51 -0800

    Add firebase into our app

---
+++ src/firebase/firebase.utils.jsx
@@ -0,0 +1,24 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+const config = {
+    apiKey: "AIzaSyA1hU7PULOgO5uN65gq7itoVfOXewqxyz8",
+    authDomain: "crwn-db-b7b9a.firebaseapp.com",
+    projectId: "crwn-db-b7b9a",
+    storageBucket: "crwn-db-b7b9a.appspot.com",
+    messagingSenderId: "873059894763",
+    appId: "1:873059894763:web:d292299cb1cd786fac85ae",
+    measurementId: "G-FTZF87WM5B"
+  }
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account' });
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  
+  export default firebase;
